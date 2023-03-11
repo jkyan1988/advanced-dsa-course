bee068e6d0860e03a9a4916ba2248b4097ccb6a7
James Kyan <james.kyan88@gmail.com>
2023-03-11 16:33:13 -0500

    Assignment started

---
+++ assignment3.js
@@ -0,0 +1,95 @@
+class MaxBinaryHeap {
+    constructor() {
+        this.values = [];
+    }
+    insert(element) {
+        this.values.push(element);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+
+        let idx = this.values.length - 1;
+
+        const element = this.values[idx];
+
+
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+
+            let parent = this.values[parentIdx];
+
+            if (element <= parent) break;
+
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+
+            idx = parentIdx;
+        }
+    }
+
+    maxValue() {
+        const max = this.values[0];
+
+        const end = this.values.pop();
+
+        if (this.values.length > 0) {
+            this.values[0] = end;
+
+            this.sinkDown()
+        }
+
+        return max;
+    }
+
+    sinkDown() {
+
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+
+            let leftChild;
+            let rightChild;
+
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (!swap && rightChild > element) ||
+                    (swap && rightChild > leftChild)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if (!swap) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+
+            idx = swap;
+        }
+    }
+}
+
+let heap = new MaxBinaryHeap();
+heap.insert(6);
+heap.insert(5);
+heap.insert(4);
+heap.insert(3);
+heap.insert(2);
+heap.insert(1);
+
+
+console.log(heap.maxValue())
---
+++ assignment3.md
@@ -0,0 +1,32 @@
+<h1>Week 3 Assignment</h1>
+
+<h2>Challenge 1: Given the following trees, label them as:</h2> 
+• Binary Search Tree<br>
+• Max Binary Heap<br>
+• Min Binary Heap<br><br>
+Tree #1 - BINARY SEARCH TREE<br>
+<img src="tree1.png" alt="Tree #1"><br>
+Tree #2 - MAX BINARY HEAP<br>
+<img src="tree2.png" alt="Tree #2"><br>
+Tree #3 - MIN BINARY HEAP<br>
+<img src="tree3.png" alt="Tree #3"><br>
+
+<h2>Challenge 2: Given the following max binary heap trees, create a function called maxValue() that returns the VALUE of the largest number in the tree. </h2> <br>
+
+<img src="maxheap.png" alt="Max Heap"><br>
+
+SOLUTION: <br>
+
+<h2>Challenge 3: Given the following min binary heap tree, create a function called minValue() that returns the 
+VALUE of the smallest number in the tree. </h2><br>
+<img src="minheap.png" alt="Min Heap"><br>
+
+SOLUTION: <br>
+
+<h2>Challenge 4: Given the following binary heap tree, create a function called isMaxHeap(heap) that accepts a 
+heap as a argument, and it should return either true or false if it’s a valid max heap tree. </h2><br>
+Example below should return TRUE<br>
+<img src="challenge4.png" alt="Challenge 4"><br>
+
+SOLUTION: <br>
+
